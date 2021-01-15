5767c1b1386cf4ec451e4469631a99c3cebae630
Vivek Dhiman <approach2vivek@gmail.com>
2021-01-15 15:55:56 +0530

    Added format and graph params

--- Git utils/GIT History.bat
+++ Git utils/GIT History.bat
@@ -15,5 +15,6 @@ cd /D %1
 REM git log --author=vdhiman >> E:\History.log
 echo & echo. >>E:\History.log
 echo Project %1 >>E:\History.log
-git log --author=vdhiman --pretty=format:%%s --after='2018-04-01T00:00:00-00:00' >>E:\History.log
+REM git log --author=vdhiman --pretty=format:%%s --after='2018-04-01T00:00:00-00:00' >>E:\History.log
+git log --author=vdhiman --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --after='2020-01-01T00:00:00-00:00' --abbrev-commit >>E:\History.log
 EXIT /B 0
